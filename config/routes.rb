97e3a47a4f50f6958a110906dd47579df48a6cf3
quasi99 <eszquasar@gmail.com>
2021-09-19 21:21:36 -0300

    config gem and dependencies (db will chage later)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+	root to: 'posts#index'
+	get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
